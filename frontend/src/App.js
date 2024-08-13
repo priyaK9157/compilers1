e934edd19a3be9e4cf0774cec3710771a6ea488e
priyaK9157 <priyakumari2003004@gmail.com>
2024-08-13 22:40:58 +0530

    first commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,7 +11,7 @@ import React, { useState, useEffect } from 'react';
 import io from 'socket.io-client';
 import Editor from '@monaco-editor/react';
 
-const socket = io('https://compilers1.onrender.com'); 
+const socket = io('https://compilers1.onrender.com')
 
 const languageOptions = [
   { value: 'python', label: 'Python' },
